920af91c28f572d32b74a60a64554f86fddd23e0
Daria <scheglova.daria@gmail.com>
2025-07-03 23:44:00 +0300

    upd: npm v, add: link in header

--- src/components/layout/HeaderApp/HeaderApp.tsx
+++ src/components/layout/HeaderApp/HeaderApp.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import Link from 'next/link'
 import { CircleUser, LogOut } from 'lucide-react'
+import { PUBLIC_PAGE } from '@/config/public-page.config'
 
 export function HeaderApp() {
     return (
         <header className="p-layout flex justify-end align-center gap-4">
-            <CircleUser />
+            <Link href={PUBLIC_PAGE.PROFILE}>
+                <CircleUser />
+            </Link>
             <LogOut />
         </header>
     )
