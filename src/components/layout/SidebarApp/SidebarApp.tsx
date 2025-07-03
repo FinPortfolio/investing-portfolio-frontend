ac6b5d333e9f5c457067285270b2076d87802201
Daria <scheglova.daria@gmail.com>
2025-07-04 00:37:25 +0300

    add: 2nd option for logo

--- src/components/layout/SidebarApp/SidebarApp.tsx
+++ src/components/layout/SidebarApp/SidebarApp.tsx
@@ -2,22 +2,46 @@ import Image from 'next/image'
 import { SidebarAppMenu } from '@/components/layout/SidebarApp/SidebarAppMenu/SidebarAppMenu'
 import Link from 'next/link'
 import { PUBLIC_PAGE } from '@/config/public-page.config'
+import { ChevronLeft } from 'lucide-react'
 
 export function SidebarApp() {
     return (
-        <aside className="max-w-[20%] h-screen bg-sidebar flex flex-col align-center shadow-custom-green p-layout">
+        <aside className="h-screen bg-sidebar flex flex-col align-start shadow-custom-green p-layout">
             <Link href={PUBLIC_PAGE.HOME}>
-                <div className="w-full relative h-[auto]" style={{ aspectRatio: '171 / 64' }}>
+                {/* <div className="w-full relative h-[auto]" style={{ aspectRatio: '171 / 64' }}>
                     <Image
-                        src="/assets/logo_full_inv_portfolio.svg"
+                        src="/assets/logo_full_inv_portfolio2.svg"
                         alt="Invest Portfolio logo full"
                         fill
                         style={{ objectFit: 'contain' }}
                     />
+                </div> */}
+                <div className="flex items-center gap-5">
+                    <div className="relative h-[auto]" style={{ aspectRatio: '57 / 64' }}>
+                        <Image
+                            src="/assets/logo_inv_portfolio.svg"
+                            alt="Invest Portfolio logo full"
+                            width={57}
+                            height={64}
+                        />
+                    </div>
+                    <div className="relative h-[auto]" style={{ aspectRatio: '101 / 34' }}>
+                        <Image
+                            src="/assets/logo_text_inv_portfolio.svg"
+                            alt="Invest Portfolio logo full"
+                            width={101}
+                            height={34}
+                        />
+                    </div>
                 </div>
-                {/* <Image src="/assets/logo_inv_portfolio.svg" alt="Invest Portfolio logo" width={57} height={64} /> */}
             </Link>
-            <SidebarAppMenu />
+            <div className="flex flex-col justify-center items-start flex-grow">
+                <button className="py-2 hover:text-primaryLight">
+                    <ChevronLeft />
+                    {/* <ChevronRight /> */}
+                </button>
+                <SidebarAppMenu />
+            </div>
         </aside>
     )
 }
--- src/components/layout/SidebarApp/SidebarAppMenu/SidebarAppMenu.tsx
+++ src/components/layout/SidebarApp/SidebarAppMenu/SidebarAppMenu.tsx
@@ -3,28 +3,20 @@
 import Link from 'next/link'
 import { SIDEBAR_DATA } from './sidebarAppMenu.data'
 import type { ISidebarAppItem } from './sidebarAppMenu.types'
-// import { ChevronLeft, ChevronRight } from 'lucide-react'
-import { ChevronLeft } from 'lucide-react'
 
 export function SidebarAppMenu() {
     return (
-        <nav className="flex flex-col my-auto text-base opacity-80">
+        <nav className="flex text-base opacity-80">
             <ul>
-                <li>
-                    <button className="py-2 hover:text-primaryLight">
-                        <ChevronLeft />
-                        {/* <ChevronRight /> */}
-                    </button>
-                </li>
                 {SIDEBAR_DATA.map((item: ISidebarAppItem, index) => (
                     <li key={index} className="hover:text-primaryLight">
                         {item.link ? (
-                            <Link href={item.link} className="flex gap-4 items-center py-2">
+                            <Link href={item.link} className="flex gap-5 items-center py-2 whitespace-nowrap">
                                 <item.icon />
                                 <span>{item.label}</span>
                             </Link>
                         ) : item.onClick ? (
-                            <button onClick={item.onClick} className="flex gap-4 items-center py-2">
+                            <button onClick={item.onClick} className="flex gap-5 items-center py-2 whitespace-nowrap">
                                 <item.icon />
                                 <span>{item.label}</span>
                             </button>
