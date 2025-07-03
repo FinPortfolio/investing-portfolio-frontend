7c3885b0fba80c6bfef9c01b3a8d8cf0e6aa1fff
Daria <scheglova.daria@gmail.com>
2025-07-04 02:11:41 +0300

    add: collapse onclick for sidebar

--- src/components/layout/SidebarApp/SidebarApp.tsx
+++ src/components/layout/SidebarApp/SidebarApp.tsx
@@ -1,31 +1,40 @@
+'use client'
+
 import Image from 'next/image'
 import { SidebarAppMenu } from '@/components/layout/SidebarApp/SidebarAppMenu/SidebarAppMenu'
 import Link from 'next/link'
 import { PUBLIC_PAGE } from '@/config/public-page.config'
-import { ChevronLeft } from 'lucide-react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { useState } from 'react'
 
 export function SidebarApp() {
+    const [collapsed, setCollapsed] = useState<boolean>(false)
+
+    const toggleSidebar = (): void => {
+        setCollapsed((prev) => !prev)
+    }
+
     return (
-        <aside className="h-screen bg-sidebar flex flex-col align-start shadow-custom-green p-layout">
+        <aside
+            className={`h-screen bg-sidebar flex flex-col align-start shadow-custom-green p-layout  transition-[width]  duration-300 ease-in-out ${
+                collapsed ? 'w-[100px]' : 'w-[240px]'
+            }`}
+        >
             <Link href={PUBLIC_PAGE.HOME}>
-                {/* <div className="w-full relative h-[auto]" style={{ aspectRatio: '171 / 64' }}>
-                    <Image
-                        src="/assets/logo_full_inv_portfolio2.svg"
-                        alt="Invest Portfolio logo full"
-                        fill
-                        style={{ objectFit: 'contain' }}
-                    />
-                </div> */}
                 <div className="flex items-center gap-5">
-                    <div className="relative h-[auto]" style={{ aspectRatio: '57 / 64' }}>
-                        <Image
-                            src="/assets/logo_inv_portfolio.svg"
-                            alt="Invest Portfolio logo full"
-                            width={57}
-                            height={64}
-                        />
-                    </div>
-                    <div className="relative h-[auto]" style={{ aspectRatio: '101 / 34' }}>
+                    <Image
+                        src="/assets/logo_inv_portfolio.svg"
+                        alt="Invest Portfolio logo full"
+                        width={36}
+                        height={40}
+                    />
+                    <div
+                        className={`transition-all duration-300 ease-in-out overflow-hidden ${
+                            collapsed
+                                ? 'opacity-0 translate-x-[-10px] w-0'
+                                : 'opacity-100 translate-x-0 w-auto delay-200'
+                        }`}
+                    >
                         <Image
                             src="/assets/logo_text_inv_portfolio.svg"
                             alt="Invest Portfolio logo full"
@@ -35,12 +44,11 @@ export function SidebarApp() {
                     </div>
                 </div>
             </Link>
-            <div className="flex flex-col justify-center items-start flex-grow">
-                <button className="py-2 hover:text-primaryLight">
-                    <ChevronLeft />
-                    {/* <ChevronRight /> */}
+            <div className="flex flex-col justify-center flex-grow">
+                <button className="py-2 hover:text-primaryLight transition-all duration-300" onClick={toggleSidebar}>
+                    {collapsed ? <ChevronRight /> : <ChevronLeft />}
                 </button>
-                <SidebarAppMenu />
+                <SidebarAppMenu collapsed={collapsed} />
             </div>
         </aside>
     )
--- src/components/layout/SidebarApp/SidebarAppMenu/SidebarAppMenu.tsx
+++ src/components/layout/SidebarApp/SidebarAppMenu/SidebarAppMenu.tsx
@@ -4,21 +4,44 @@ import Link from 'next/link'
 import { SIDEBAR_DATA } from './sidebarAppMenu.data'
 import type { ISidebarAppItem } from './sidebarAppMenu.types'
 
-export function SidebarAppMenu() {
+interface SidebarAppMenuProps {
+    collapsed: boolean
+}
+
+export function SidebarAppMenu({ collapsed }: SidebarAppMenuProps) {
     return (
-        <nav className="flex text-base opacity-80">
+        <nav className="flex item-start text-base opacity-80 w-full transition-all duration-300">
             <ul>
                 {SIDEBAR_DATA.map((item: ISidebarAppItem, index) => (
                     <li key={index} className="hover:text-primaryLight">
                         {item.link ? (
-                            <Link href={item.link} className="flex gap-5 items-center py-2 whitespace-nowrap">
+                            <Link
+                                href={item.link}
+                                className={`relative flex py-2 transition-all duration-300 ${collapsed ? '' : 'gap-5'}`}
+                            >
                                 <item.icon />
-                                <span>{item.label}</span>
+                                <span
+                                    className={`whitespace-nowrap overflow-hidden transition-all duration-300 ease-in-out
+    ${collapsed ? 'opacity-0 translate-x-[-10px] delay-0' : 'opacity-100 translate-x-0 delay-200'}
+
+  `}
+                                >
+                                    {item.label}
+                                </span>
                             </Link>
                         ) : item.onClick ? (
-                            <button onClick={item.onClick} className="flex gap-5 items-center py-2 whitespace-nowrap">
+                            <button
+                                onClick={item.onClick}
+                                className={`flex py-2 transition-all duration-300 ${collapsed ? '' : 'gap-5'}`}
+                            >
                                 <item.icon />
-                                <span>{item.label}</span>
+                                <span
+                                    className={`whitespace-nowrap overflow-hidden transition-all duration-300 ease-in-out
+    ${collapsed ? 'opacity-0 translate-x-[-10px] delay-0' : 'opacity-100 translate-x-0 delay-200'}
+  `}
+                                >
+                                    {item.label}
+                                </span>
                             </button>
                         ) : null}
                     </li>
